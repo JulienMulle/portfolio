bbac2539fdb36d89e12d073e11670186e846e7ad
Julien <julienmulle@gmail.com>
2021-09-29 10:30:04 +0200

    ajout des routes contact, home, portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
  import React from 'react'
+import { Route, Switch } from 'react-router';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
  
  const App = () => {
    return (
-     <div>
-       App
-     </div>
+     <Switch>
+       <Route exact path="/" component={Home}/>
+       <Route exact path="/project-1" component={Portfolio} />
+       <Route exact path="/contact" component={Contact} />
+     </Switch>
    )
  }
  
