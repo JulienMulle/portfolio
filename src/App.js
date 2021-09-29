5a0fc382eafdfd2f88f4559a950d4038d38d055b
Julien <julienmulle@gmail.com>
2021-09-29 11:06:52 +0200

    changement portfolio en project + maj route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import { Redirect, Route, Switch } from 'react-router';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
-import Portfolio from './pages/Portfolio';
+import { Project1, Project2, Project3, Project4 } from './pages/Projects';
  
  const App = () => {
    return (
      <Switch>
        <Route exact path="/" component={Home}/>
-       <Route exact path="/portfolio" component={Portfolio} />
+       <Route exact path="/project-1" component={Project1} />
+       <Route exact path="/project-2" component={Project2} />
+       <Route exact path="/project-3" component={Project3} />
+       <Route exact path="/project-4" component={Project4} />
        <Route exact path="/contact" component={Contact} />
        <Redirect to path="/" />
      </Switch>
---
+++ src/pages/Projects.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export const Project1 = () => {
+    return (
+        <main>
+            <div className="project">
+                Projet 1
+            </div>
+        </main>
+    )
+};
+
+export const Project2 = () => {
+    return (
+        <main>
+            <div className="project">
+                Projet 2
+            </div>
+        </main>
+    )
+};
+export const Project3 = () => {
+    return (
+        <main>
+            <div className="project">
+                Projet 3
+            </div>
+        </main>
+    )
+};
+
+export const Project4 = () => {
+    return (
+        <main>
+            <div className="project">
+                Projet 4
+            </div>
+        </main>
+    )
+};
