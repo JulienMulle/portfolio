77fe33d43d4be326cccb566b2c74bdde07ef100b
Julien <julienmulle@gmail.com>
2021-09-30 16:19:57 +0200

    Update Mouse.js

--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -2,22 +2,24 @@ import React, { useEffect } from 'react';
 
 const Mouse = () => {
 
+    const cursor = document.querySelector('.cursor');
+
     useEffect(() => {
         //on souhaite que le cercle suive la souris dès qu'elle ce deplace
         const handleCursor = (e) => {
-            document.querySelector('.cursor').style.top = e.pageY + "px";
-            document.querySelector('.cursor').style.left = e.pageX + "px";
+            cursor.style.top = e.pageY + "px";
+            cursor.style.left = e.pageX + "px";
         }
 
         window.addEventListener('mousemove', handleCursor);
         //quand la souris et le cercle sont sur un lien, le cercle change de forme
         const handleHover = () => {
             //on rajoute une classe pour retrecir le cercle sur un lien
-            document.querySelector('.cursor').classList.add('hovered');
+            cursor.classList.add('hovered');
         };
         const handleLeave =() => {
-            //et on lui la classe avec une transition quand elle n'est plus sur un lien
-            document.querySelector('.cursor').style.transition = ".3s ease-out";
+            //et on lui enlève la classe avec une transition quand elle n'est plus sur un lien
+            cursor.style.transition = ".3s ease-out";
             document.querySelector('.cursor').classList.remove('hovered');
         }
         document.querySelectorAll('.hover').forEach((link) => {
