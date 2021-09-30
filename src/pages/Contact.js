30b79c2e14b11c19639d91fe42adb4ee86cac8d2
Julien <julienmulle@gmail.com>
2021-09-30 16:53:08 +0200

    Maj page contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,61 @@
 import React from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import ButtonsBottom from '../components/ButtonsBottom';
+import Logo from '../components/Logo';
 import Mouse from '../components/Mouse';
 import Navigation from '../components/Navigation';
+import SocialeNetwork from '../components/SocialeNetwork';
 
 
 const Contact = () => {
     return (
-        <div>
+        <main>
             <Mouse />
+            <div className="contact">
             <Navigation/>
-            contact
+            <Logo />
+            <div className="contact-infos">
+                <div className="adress">
+                    <div className="content">
+                        <h4>adresse</h4>
+                        <p> 12 rue du code</p>
+                        <p> 33000 Bordeaux</p>
+                    </div>
+                </div>
+
+                <div className="phone">
+                    <div className="content">
+                        <h4>téléphone</h4>
+                        <CopyToClipboard text="0606060606" className="hover">
+                            <p style={{cursor: 'pointer'}} 
+                            className="clipboard" 
+                            onClick={() => {
+                                alert('le numéro est copié !');
+                            }}> 
+                            0619408776</p>
+                        </CopyToClipboard>
+                    </div>
+                </div>
+
+                <div className="email">
+                    <div className="content">
+                        <h4>email</h4>
+                        <CopyToClipboard text="julienmulle@gmail.com"
+                        className="hover">
+                            <p style={{cursor: 'pointer'}} 
+                            className="clipboard" 
+                            onClick={() => {
+                                alert(`l'Email est copié !`);
+                            }}> 
+                            julienmulle@gmail.com</p>
+                        </CopyToClipboard>
+                    </div>
+                </div>
+                <SocialeNetwork />
+            </div>           
             <ButtonsBottom/>
-        </div>
+            </div>
+        </main>
     )
 }
 
