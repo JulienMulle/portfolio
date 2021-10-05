4cc09c28dea8a5664a2d77083c5b96ca93b2053e
Julien <julienmulle@gmail.com>
2021-10-05 10:57:23 +0200

    Mise en place du scroll horizontale

--- src/App.js
+++ src/App.js
@@ -1,19 +1,76 @@
- import React from 'react'
-import { Redirect, Route, Switch } from 'react-router';
+import React, {useEffect} from 'react'
+import { Redirect, Route, Switch, useHistory, useLocation } from 'react-router';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import { Project1, Project2, Project3, Project4 } from './pages/Projects';
  
  const App = () => {
+
+  //navigation pour scroller de gauche à droite et de droite à gauche, au lieu du traditionnelle vers le bas ou le haut
+    const location = useLocation();
+    const history = useHistory();
+
+    useEffect(() => {
+      const handleScrollToElement = (e)=>{
+        const url = window.location.origin + "/";
+
+        const wheelRouter = (after, before) => {
+          if (e.wheelDeltaY < 0) {
+            setTimeout(()=>{
+              history.push(after)
+            }, 500);
+
+          } else if (e.wheelDeltaY > 0)  {
+            setTimeout(()=>{
+              history.push(before)
+            }, 500);
+          }
+        }
+      
+        switch (window.location.href.toString()) {
+          case url:
+            if (e.wheelDeltaY <0){
+              setTimeout(()=>{
+                history.push('projet-1')
+              }, 500);     
+          }
+            break;
+          case url +"projet-1":
+            wheelRouter('projet-2', '');
+            break;
+          case url +"projet-2":
+             wheelRouter('projet-3', 'projet-1');
+            break;
+          case url +"projet-3":
+            wheelRouter('projet-4', 'projet-2');
+            break;
+          case url +"projet-4":
+            wheelRouter('contact', 'projet-3');
+            break;
+          case url +"contact":
+            if(e.wheelDeltaY >0){
+              setTimeout(()=>{
+                history.push('projet-4')
+              }, 500);
+            }  
+            break;
+          default:
+          console.log(`t'inquiete, tout est sous controle !...`);
+        }
+      };
+
+      window.addEventListener('wheel', handleScrollToElement) 
+    }, [history]);
+
    return (
-     <Switch>
+     <Switch location={location} key={location.pathname}>
        <Route exact path="/" component={Home}/>
        <Route exact path="/projet-1" component={Project1} />
        <Route exact path="/projet-2" component={Project2} />
        <Route exact path="/projet-3" component={Project3} />
        <Route exact path="/projet-4" component={Project4} />
        <Route exact path="/contact" component={Contact} />
-       <Redirect to path="/" />
+       <Redirect to path="/" /> 
      </Switch>
    )
  }
