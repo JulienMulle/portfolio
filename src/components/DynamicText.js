26df5b1e64b1f334d9fe739e8f61e74035d34920
Julien <julienmulle@gmail.com>
2021-12-03 12:37:30 +0100

    correction divers bug

--- src/components/DynamicText.js
+++ src/components/DynamicText.js
@@ -51,7 +51,7 @@ const DynamicText = () => {
       
     return (
         <span className="dynamic-text">
-            <span className="simply">skills</span>
+            <span className="simply">Techno empl </span>
             <span id="text-target"></span>
         </span>
     )
