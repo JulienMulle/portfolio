8256df3b3037e600e132b9d66a5815c5f59d4e42
Julien <julienmulle@gmail.com>
2021-12-06 12:53:32 +0100

    suppression de DynamicText

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ButtonsBottom from '../components/ButtonsBottom';
-import DynamicText from '../components/DynamicText';
 import Mouse from '../components/Mouse';
 import Navigation from '../components/Navigation';
 import SocialeNetwork from '../components/SocialeNetwork.js';
@@ -45,7 +44,6 @@ const Home = () => {
                 <h2>Hello, je suis </h2>
                 <h1> Julien Mulle</h1>
                 <h2>Developpeur Front-End</h2>
-                {/*<h2><DynamicText /></h2>*/}
                 </div>
             </div>
             <ButtonsBottom  right={"/projet-1"}/>
